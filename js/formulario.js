7c5d5778663d6ba0fa440b97a2064de3929275e8
jeisongarcia11 <74687446+jeisongarcia11@users.noreply.github.com>
2021-05-01 16:29:21 -0500

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -74,6 +74,7 @@ formulario.addEventListener('submit', (e) => {
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
 		setTimeout(() => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+			document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 		}, 5000);
 
 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
